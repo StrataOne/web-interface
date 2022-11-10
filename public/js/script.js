a7c3c6b834ff1c167a704d369f8868ef95f8f41a
Pureheart Moluno <39631660+pmoluno@users.noreply.github.com>
2022-11-10 11:01:47 +0100

    Updated the UI Network Card
    I have Updated the UI Network Card to match the required design.
    I have also added some other sections to the UI.

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,27 @@
+// ANIMATE ON SCROLL
 AOS.init();
+// STATISTICS COUNT DOWN
+function animate(obj, initVal, lastVal, duration) {
+    let startTime = null;
+    let currentTime = Date.now();
+    const step = (currentTime ) => {
+        if (!startTime) {
+        startTime = currentTime ;
+        }
+        const progress = Math.min((currentTime  - startTime) / duration, 1);
+        obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
+        if (progress < 1) {
+            window.requestAnimationFrame(step);
+        }
+    };
+    window.requestAnimationFrame(step);
+}
+let text1 = document.getElementById('amount-deligated');
+let text2 = document.getElementById('paid-reward');
+let text3 = document.getElementById('delegates');
+if(  $("#statistics").is(":visible") == true )
+{  
+    animate(text1, 0, 3845866, 5000);
+    animate(text2, 0, 38482, 5000);
+    animate(text3, 100, 3845777, 5000);    
+}
